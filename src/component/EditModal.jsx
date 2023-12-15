0e6d1f95a453e98eb8b51e541905d79cc09f9784
akashmishra0908 <112822976+akashmishra0908@users.noreply.github.com>
2023-12-15 15:25:35 +0530

    completed

--- src/component/EditModal.jsx
+++ src/component/EditModal.jsx
@@ -31,7 +31,6 @@ const EditModal = ({
           <option value="">Select a reason</option>
           <option value="reason1">Missing Product </option>
           <option value="reason1">Price is not the same </option>
-          <option value="reason2">Other</option>
         </select>
       </label>
       <br />
